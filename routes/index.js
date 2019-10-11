3e9de8a5fd8c8f05b8716fb9d94af20156b1d022
ajaxwd <ajaxwd@gmail.com>
2019-10-10 22:21:41 -0300

    vistas rutas controles

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const proyectosControllers = require('../controllers/proyectosControllers');
+
+module.exports = function(){
+
+    router.get('/', proyectosControllers.proyectosHome);
+    return router;
+}
