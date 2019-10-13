912d260c94398b0a2ddb61679ea11943c23597a5
ajaxwd <ajaxwd@gmail.com>
2019-10-13 18:27:04 -0300

    URL amigables

--- controllers/proyectosControllers.js
+++ controllers/proyectosControllers.js
@@ -1,4 +1,5 @@
 const Proyectos = require('../model/Proyectos');
+const slug = require('slug');
 
 exports.proyectosHome = (req, res) => {
     res.render('index', {
@@ -28,7 +29,8 @@ exports.nuevoProyecto = async (req, res) => {
             errores
         })
     } else {
-        const proyecto = await Proyectos.create({ nombre });
+        const url = slug(nombre).toLowerCase();
+        const proyecto = await Proyectos.create({ nombre, url });
         res.redirect('/');
     }
 }
