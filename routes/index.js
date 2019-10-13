c78205445a1d5ec33dbee305023b720c798885ec
ajaxwd <ajaxwd@gmail.com>
2019-10-13 00:34:42 -0300

    validador controlador nuevo proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
+const { body } = require('express-validator/check');
+
 const proyectosControllers = require('../controllers/proyectosControllers');
+
+
 
 module.exports = function(){
 
     router.get('/', proyectosControllers.proyectosHome);
     router.get('/nuevo-proyecto', proyectosControllers.formularioProyecto);
-    router.post('/nuevo-proyecto', proyectosControllers.nuevoProyecto);
+    router.post('/nuevo-proyecto', 
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectosControllers.nuevoProyecto);
     return router;
 }
